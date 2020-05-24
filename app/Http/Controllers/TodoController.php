e8ad6f98fde9755ad061ae1557e054b750eec389
DESKTOP-T4MR9R6\saya <saya.jojolocker@gmail.com>
2020-05-24 19:21:01 +0900

    Laravelの環境構築・初期設定
    ・環境構築
    ・ルーティング設定
    ・コントローラーの実装
    　定義した変数の描画

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    //
+    public function index()
+    {
+        $number_list = range(0,100,10);
+        return view('welcome', ['name' => 'chanman',
+            'address' => 'oosakahu',
+            'list' => $number_list
+            ]);
+
+    }
+}
