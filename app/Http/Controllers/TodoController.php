3c50a097e58310f9f31299876d588e1a569e8160
DESKTOP-T4MR9R6\saya <saya.jojolocker@gmail.com>
2020-05-30 01:55:42 +0900

    Eloquentの作成 usersレコードの取得、編集、追加、削除の学習

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class TodoController extends Controller
 {
     //
     public function index()
     {
+        //$users = User::where('email','like','%@example.org')->orderBy('id','desc')->get();
+        $user = User::find(11);
+        $user->delete();
+        dd($user);
+
+
         $number_list = range(0, 100, 10);
         return view('welcome', ['name' => 'chanman',
             'address' => 'oosakahu',
