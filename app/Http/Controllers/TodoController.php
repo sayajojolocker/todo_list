d4ea3f67deed85b1bb427eaab60f1b8712e72ed3
DESKTOP-T4MR9R6\saya <saya.jojolocker@gmail.com>
2020-06-03 02:49:09 +0900

    todolist画面作成 todolistsテーブルの追加

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todolists;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -12,17 +13,19 @@ class TodoController extends Controller
     public function index()
     {
         //$users = User::where('email','like','%@example.org')->orderBy('id','desc')->get();
-        $user = User::find(11);
-        $user->delete();
-        dd($user);
+//        $user = User::find(11);
+//        $user->delete();
+//        dd($user);
+//
+//
+//        $number_list = range(0, 100, 10);
+//        return view('welcome', ['name' => 'chanman',
+//            'address' => 'oosakahu',
+//            'list' => $number_list
+//        ]);
 
-
-        $number_list = range(0, 100, 10);
-        return view('welcome', ['name' => 'chanman',
-            'address' => 'oosakahu',
-            'list' => $number_list
-        ]);
-
+        $todo_list = Todolists::all();
+        return view('index', ['todolist' => $todo_list]);
     }
 
     public function test()
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,29 @@
 
 @extends('layout')
-@section('script')
-    <script>
 
-    </script>
+
+@section('title')
+TODOLIST
 @endsection
-
 
 @section('contents')
-<h1>index</h1>
+
+    <form>
+        <div class="input-group">
+            <div class="input-group-append">
+                <input type="submit" class="btn btn-secondary" value="追加">
+            </div>
+            <input type="text" name="todo" value="">
+        </div>
+        <div>
+            @foreach ($todolist as $item)
+                <li>{{ $item['todo'] }}</li>
+            @endforeach
+        </div>
+    </form>
+
+
 @endsection
 
-{{$sw}}
+
 
